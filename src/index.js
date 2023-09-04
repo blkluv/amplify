0e71587c9fd55ffb7eb19a7b7cbb6838b8eb677d
blkluv <info@blkluv.org>
2023-09-04 19:57:25 -0400

    node

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Amplify } from "aws-amplify";
-import config from "./aws-exports";
 import { AmplifyProvider, ThemeProvider } from "@aws-amplify/ui-react";
 import { studioTheme } from "./ui-components";
 import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
 
-Amplify.configure(config);
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -22,7 +22,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
